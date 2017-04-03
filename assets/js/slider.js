07bb0e7d89e62b9335cf6bb0b4b9a3e3baac8796
Alexandre Levy <levy_a@etna-alternance.net>
2017-04-03 20:34:50 +0200

    Add content to ninja page

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Heffiros on 03/04/2017.
+ */
+$(document).ready(function() {
+    $('.slider-ninja').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
